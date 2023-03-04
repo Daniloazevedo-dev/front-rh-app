422c345b714cf78f659aa5a3ad81f8edf95dcb1a
Damaris <d.pereira-ato@parceirosedu.com.br>
2023-03-04 17:05:56 -0300

    ajuste css componente profissão

--- src/app/profissao/profissao.component.ts
+++ src/app/profissao/profissao.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ProfissaoService} from "../service/profissao.service";
+import {Table} from "primeng/table";
 
 @Component({
   selector: 'app-profissao',
@@ -10,6 +11,8 @@ export class ProfissaoComponent implements OnInit {
   profissao: any;
   msgError: any;
 
+  @ViewChild('filter') filter!: ElementRef;
+
   constructor(
     private profissaoService: ProfissaoService,
   ) {
@@ -19,6 +22,15 @@ export class ProfissaoComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
+
+  clear(table: Table) {
+    table.clear();
+    this.filter.nativeElement.value = '';
   }
 
   buscarProfissao() {
