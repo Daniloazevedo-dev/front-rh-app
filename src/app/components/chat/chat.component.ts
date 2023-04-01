1b1bd43078d84c1036f31949d2b5e5f35a74ee1f
Danilo Azevedo <danilo.ann@gmail.com>
2023-04-01 19:37:59 -0300

    Ajuste primeiro acesso e chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BlipChat } from 'blip-chat-widget';
+import { TokenService } from 'src/app/service/token.service';
+import { UsuarioService } from 'src/app/service/usuario.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,17 +9,66 @@ import { BlipChat } from 'blip-chat-widget';
   styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent implements OnInit {
-  constructor() {}
+
+  usuario: any;;
+
+  constructor(private tokentService: TokenService,
+              private usuarioService: UsuarioService) {}
+
 
   ngOnInit() {
-    localStorage.removeItem('blipSdkUAccount');
-    new BlipChat()
-      .withAppKey('ZmVuaXg3OmEzYjUwZDRjLWQyMjktNGMyYy05NjZhLWRhNTM3MWE5NWMwMQ==')
-      .withButton({ color: '#2CC3D5'})
-      .withCustomCommonUrl('https://danilo-azevedo-fezft.chat.blip.ai/')
-      .build();
+    this.buscaUsuarioEmail();
+  }
+
+     chamarCharBot(data) {
+
+      localStorage.removeItem('blipSdkUAccount');
+
+      var primeiroAcesso = this.validaPrimeiroAcesso(data.primeiroAcesso);
+      console.log(primeiroAcesso);
+      var id = data.id;
+      var nome = data.nome;
+      var token = this.tokentService.getToken();
+
+      (function() {
+        window.onload = function() {
+          var blipClient = new BlipChat();
+          blipClient
+              .withAppKey(
+                  'ZmVuaXg3OmEzYjUwZDRjLWQyMjktNGMyYy05NjZhLWRhNTM3MWE5NWMwMQ==')
+              .withCustomCommonUrl('https://danilo-azevedo-fezft.chat.blip.ai/')
+              .withEventHandler(BlipChat.LOAD_EVENT,
+                  function() {
+                    blipClient.sendMessage({
+                      "type" : "text/plain",
+                      "content" : {id: id, token: token, nome: nome, primeiroAcesso: primeiroAcesso},
+                      "metadata":{
+                        "#blip.hiddenMessage": true
+                       }
+                    });
+                  }).build();
+
+                  if(primeiroAcesso) {
+                    blipClient.toogleChat();
+                  }
+        }
+
+      })();
 
      }
 
+
+     buscaUsuarioEmail() {
+      this.usuarioService
+        .buscarUsuarioEmail(this.tokentService.getUserName())
+        .subscribe(
+          (data) => {
+            this.chamarCharBot(data);
+          });
+     }
+
+     validaPrimeiroAcesso(primeiroAcesso) {
+      return primeiroAcesso ? primeiroAcesso : false;
+     }
 
 }
