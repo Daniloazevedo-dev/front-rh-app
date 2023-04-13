13b0de67ac478328fea72598936cf2278b68e8c6
Luiz Generoso <luizerytre@gmail.com>
2023-04-13 15:38:19 -0300

    Ajuste colaborador lançamento

--- src/app/components/colaborador/lancamento/lancamento.component.ts
+++ src/app/components/colaborador/lancamento/lancamento.component.ts
@@ -27,6 +27,16 @@ export class LancamentoColComponent extends FormBase implements OnInit {
   valorDia: any;
   colId: any;
   nomeCol: any;
+  statusOptions: any[];
+  value1: string = "0";
+  value2: number;
+  value3: number;
+  pagamentoAguardando: any;
+  pagamentoReprovado: any;
+  pagamentoAprovado: any;
+  botaoLabel: string;
+  botaoEstilo: string;
+  botaoStatus: boolean;
 
   @ViewChild('filter') filter!: ElementRef;
 
@@ -41,6 +51,10 @@ export class LancamentoColComponent extends FormBase implements OnInit {
 
     super();
     this.colaborador = this.buscaUsuarioLogadoPorEmail();
+    this.statusOptions = [{label: 'Aguardando', value: '0'}, {label: 'Aprovado', value: '1'}, {
+      label: 'Reprovado',
+      value: '2'
+    }];
 
   }
 
@@ -48,6 +62,25 @@ export class LancamentoColComponent extends FormBase implements OnInit {
     this.setForm();
     this.buscaUsuarioLogadoPorEmail();
 
+  }
+
+  pagamentoFiltro(status) {
+    if (status === '0') {
+      this.botaoLabel = 'Aguardando';
+      this.botaoEstilo = 'p-button-secondary';
+      this.botaoStatus = true;
+      this.pagamento = this.pagamentoAguardando
+    } if (status === '1') {
+      this.botaoLabel = 'Aprovado';
+      this.botaoEstilo = 'p-button-info';
+      this.botaoStatus = false;
+      this.pagamento = this.pagamentoAprovado
+    } if (status === '2') {
+      this.botaoLabel = 'Reprovado';
+      this.botaoEstilo = 'p-button-info';
+      this.botaoStatus = false;
+      this.pagamento = this.pagamentoReprovado
+    }
   }
 
   private setForm() {
@@ -101,7 +134,10 @@ export class LancamentoColComponent extends FormBase implements OnInit {
     this.colaboradorService.listaPagamentoIdCol(id).subscribe(
       (pagamento) => {
         this.pagamento = pagamento;
-        // console.log(pagamento['id'])
+        this.pagamentoAguardando = this.pagamento.filter(p => p.status === '0')
+        this.pagamentoAprovado = this.pagamento.filter(p => p.status === '1')
+        this.pagamentoReprovado = this.pagamento.filter(p => p.status === '2')
+        this.pagamento = this.botaoStatus === true ? this.pagamentoAguardando : this.pagamentoAguardando;
       },
       (error) => {
         this.msgError = [
