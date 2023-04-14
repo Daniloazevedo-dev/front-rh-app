bae196fb9781ddcb9dc00b0f1e0eab7cf4405ca0
Luiz Generoso <luizerytre@gmail.com>
2023-04-14 14:05:58 -0300

    Ajuste pagamento.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  // @ts-ignore
+  API_URL: process.env['API_URL'],
+  // @ts-ignore
+  OAUTH_CLIENT: process.env['OAUTH_CLIENT'],
+  // @ts-ignore
+  OAUTH_SECRET: process.env['OAUTH_SECRET'],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,9 @@
 export const environment = {
-  production: false,
+  production: true,
   // @ts-ignore
   API_URL: process.env['API_URL'],
   // @ts-ignore
   OAUTH_CLIENT: process.env['OAUTH_CLIENT'],
   // @ts-ignore
   OAUTH_SECRET: process.env['OAUTH_SECRET'],
- };
-
-
+};
